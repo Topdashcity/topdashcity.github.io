8ab5faca6dd4727ff25e701960d59b0461692ac6
El Aguacate Programador <grilletjhony@gmail.com>
2023-09-20 22:38:14 -0400

    Add types for social data and logo

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,6 +2,7 @@ import { defineCollection, z } from 'astro:content'
 
 const commerceCollection = defineCollection({ 
   schema: z.object({
+    logo: z.string().optional(),
     name: z.string(),
     tag: z.string(),
     city: z.string(),
@@ -10,6 +11,14 @@ const commerceCollection = defineCollection({
     isSubsidiary: z.boolean(),
     isMainSubsidiary: z.boolean(),
     subsidiaryFrom: z.string().optional(),
+    social: z.object({
+      whatsapp: z.string().optional(),
+      telephone: z.number().optional(),
+      facebook: z.string().optional(),
+      twitter: z.string().optional(),
+      instagram: z.string().optional(),
+      email: z.string().optional()
+    }).optional()
   })
 })
 
