b8cd82f5b37d42b3d10102a15cd2ec1775765cb7
El Aguacate Programador <grilletjhony@gmail.com>
2023-09-26 11:49:04 -0400

    Add search page and search engine

---
+++ src/components/vue/Card.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+  tag: { type: String, default: 'Tag' }, 
+	name: { type: String, default: 'Commerce' }, 
+	address: { type: String, default: 'This is the address' },
+	logo: { type: String, default: 'https://via.placeholder.com/192x192.png?text=Logo+Not+Available' },
+	cta: { type: String, default: '/directorio' }
+})
+</script>
+
+<template>
+  <div class="w-[20rem] h-[40rem] m-8 p-8 bg-slate drop-shadow-lg text-center">
+    <div class="flex flex-col justify-between h-full">
+      <div>
+        <img class="mx-auto mb-8 w-48" :src="props.logo" alt="Logo">
+        <div class="uppercase font-bold tracking-widest rounded-full bg-accent text-white h-12 py-3">
+          {{ props.tag }}
+        </div>
+        <p class="font-bold tracking-widest">{{ props.name }}</p>
+        <p class="text-sm">{{ props.address }}</p>
+      </div>
+      <div>
+        <a class="button rounded-lg" :href="props.cta">
+          Ver más
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/vue/SearchEngine.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref, computed } from 'vue'
+import Fuse from 'fuse.js'
+import Card from './Card.vue';
+
+const props = defineProps({
+  searchIndex: Array
+})
+
+const searchInput = ref('')
+const searchResults = ref(props.searchIndex)
+
+const searchIsEmpty = computed(() => (searchResults.value.length === 0))
+const inputIsEmpty = computed(() => searchInput.value === '')
+
+const fuseOptions = {
+  keys: ['data.name', 'data.address', 'data.tag', 'data.city'],
+	includeMatches: true,
+	minMatchCharLength: 3,
+	threshold: 0.3,
+}
+
+const searchList = new Fuse(props.searchIndex, fuseOptions)
+
+const search = () => {
+  searchResults.value = (inputIsEmpty.value) 
+    ? props.searchIndex
+    : searchResults.value = searchList
+        .search(searchInput.value)
+        .map((searchItem) => (searchItem.item))
+  searchInput.value = ''
+}
+</script>
+
+<template>
+  <section class="mx-auto py-6 w-2/3 flex justify-center place-items-center">
+    <input v-model="searchInput" placeholder="🔍 Escribe tú búsqueda" @keyup.enter="search" class="w-2/3 h-[3rem] bg-slate pr-8" type="text" />
+    <button class="-ml-4 button rounded-r-lg search" @click="search">Buscar</button>
+  </section>
+  <div v-if="searchIsEmpty">
+    <h2 class="text-primary text-center">No se ha encontrado su búsqueda.</h2>
+    <p class="text-center">Puedes intentar buscando en el <a class="text-primary font-bold" href="/directorio">directorio</a></p>
+  </div>
+  <section class="grid gap-4 grid-cols-4" v-else>
+    <Card 
+      v-for="commerce in searchResults"
+      v-bind="commerce.data"
+      :cta="`/directorio/${commerce.data.city}/${commerce.slug}`"
+    />
+  </section>
+</template>
