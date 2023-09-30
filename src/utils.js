c3ab3ceb708ff61b315c54803950b6739b3d7fbe
El Aguacate Programador <grilletjhony@gmail.com>
2023-09-29 22:06:58 -0400

    Export static data to utils library

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,101 @@
 export const cities = ['caracas', 'margarita', 'valencia']
+
+export const menuLinks = [
+  ['Directorio', '/directorio'],
+  ['Eventos', '/eventos'],
+  ['Nosotros', '/nosotros']
+]
+
+export const features = [
+  {
+    image: '/wallets/comision-baja.png',
+    text: '<span class="ml-4">Comisiones de <span class="font-bold">$0.002</span> por transacción.</span>'
+  },
+  {
+    image: '/wallets/rapidez.png',
+    text: '<span class="ml-4">Menos de <span class="font-bold">2 segundos</span>.</span>'
+  },
+  {
+    image: '/wallets/seguridad.png',
+    text: '<span class="ml-4">Transacciones <span class="font-bold">irreversibles</span></span>'
+  }
+]
+
+export const installerLinks = [
+  {
+    link: 'https://apps.apple.com/app/dash-wallet/id1206647026',
+    image: '/botones_05.png'
+  },
+  {
+    link: 'https://play.google.com/store/apps/details?id=hashengineering.darkcoin.wallet&referrer=utm_source%3Ddash-org%26utm_medium%3Ddownloads%26utm_campaign%3Dmobile',
+    image: '/botones_03.png'
+  }
+]
+
+export const exchangeLinks = [
+  {
+    link: 'https://www.binance.com/es-MX',
+    image: '/exchanges/botones_09.png'
+  },
+  {
+    link: 'https://coincoinx.com/',
+    image: '/exchanges/botones_16.png'
+  },
+  {
+    link: 'https://cryptobuyerlatam.com/en/',
+    image: '/exchanges/botones_06.png'
+  },
+  {
+    link: 'https://afx.trade/',
+    image: '/exchanges/botones_04.png'
+  }
+]
+
+export const phoneLinks = [
+  {
+    class: 'h-[20rem] sm:h-[32rem]',
+    image: '/wallets/dash-wallet.png',
+    link: 'https://www.dash.org/downloads/'
+  },
+  {
+    class: 'h-[20rem] sm:h-[32rem] pt-16',
+    image: '/wallets/binance.png',
+    link: 'https://www.binance.com/es-MX'
+  },
+  {
+    class: 'h-[20rem] sm:h-[32rem] pt-32',
+    image: '/wallets/trust.png',
+    link: 'https://trustwallet.com/es/'
+  },
+  {
+    class: 'h-[20rem] sm:h-[32rem]',
+    image: '/wallets/uphold.png',
+    link: 'https://uphold.com/es'
+  },
+  {
+    class: 'h-[20rem] sm:h-[32rem] pt-16',
+    image: '/wallets/coinomi.png',
+    link: 'https://www.coinomi.com/en/'
+  },
+  {
+    class: 'h-[20rem] sm:h-[32rem] pt-32',
+    image: '/wallets/exodus.png',
+    link: 'https://www.exodus.com/'
+  }
+]
+
+export const eventos = [{
+  title: 'DasherPay',
+  content: 'DasherPay, una herramienta creada para ayudar a las empresas en la realización de pagos masivos con Dash a su personal, fue la protagonista en llevar a varios comerciantes de la ciudad de Caracas y Margarita la oportunidad de encaminar, desde cero, a cada uno de sus colaboradores hacia el mundo Cripto.',
+  image: '/events/IMG_0706.png',
+  hasCTA: false
+}, {
+  title: 'Carrera BioMercados 10K',
+  content: 'El pasado 14 de noviembre de 2021, se llevó a cabo la 7.ª edición de la Carrera 10K de BioMercados, y Dash se hizo presente para llevar emoción y cripto-premios a los participantes.',
+  image: '/events/IMG_1916.jpg',
+  hasCTA: false
+}, {
+  title: 'Dash Point Sambil',
+  content: 'Año tras año, los Dash Point se han convertido en una de las actividades más solicitadas por nuestros aliados comerciales. Es que sin dudas, tener donde comprar Dash hasta con PagóMóvil, y aprovechar de comprar productos con descuentos, todo en un solo lugar, es una maravilla.',
+  hasCTA: false
+}]
